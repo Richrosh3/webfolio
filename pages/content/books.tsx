1babdedf8153d8b5f7005858e6a5258daac1a158
Roshen Abraham <richrosh3@gmail.com>
2023-05-15 17:50:41 -0400

    Completed version 0 of Books page. I was able to remove Overall Score from the reviews and display it on its own. Added Title and Score header. Changed up UI for this page. There's still more I want to do on this page, but for now I'll leave it as is.

--- pages/content/books.tsx
+++ pages/content/books.tsx
@@ -27,35 +27,64 @@ const Books: NextPage<{
   return (
     <div className="justify-between items-center bg-gradient-to-b from-slate-600 to-slate-900 p-10 w-screen h-max">
       <NavBar />
-      <div className="py-20 ">
+      <div className="flex flex-row pt-5 border-b-2 border-white pb-2">
+        <div className="w-1/3 h-full">
+          <p className = "font-bold font-sans text-[#b3f2ff] text-3xl pl-3">Title</p>
+        </div>
+        <div className="w-1/3"></div>
+        <div className="w-1/3 justify-center text-center">
+          <p className = "font-bold font-sans text-[#b3f2ff] text-3xl">Score</p>
+        </div>
+      </div>
+      <div className="pt-3 ">
         {props.books.map((data, index) => {
           const panel = "panel" + (index + 1).toString();
           const ariaControls = panel + "bh-content";
           const panelID = panel + "2bh-header";
+
+          if (data.rating == "did not like it") {
+            data.rating = "1";
+          } else if (data.rating == "it was ok") {
+            data.rating = "2";
+          } else if (data.rating == "liked it") {
+            data.rating = "3";
+          } else if (data.rating == "really liked it") {
+            data.rating = "4";
+          } else if (data.rating == "it was amazing") {
+            data.rating = "5";
+          }
+
+          let [review, overallScore] = data.review.split("Overall Score: ");
+
           return (
             <Accordion
               expanded={expanded === panel}
               onChange={handleChange(panel)}
+              className="bg-inherit shadow-sm shadow-[#b3f2ff]/50"
             >
               <AccordionSummary
-                className="bg-gradient-to-r from-emerald-200 to-blue-400"
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls={ariaControls}
                 id={panelID}
               >
-                <Typography className = "font-bold text-emerald-500" sx={{ width: "33%", flexShrink: 0 }}>
+                <Typography
+                  className="font-bold font-sans text-[#b3f2ff] text-lg"
+                  sx={{ width: "84%", flexShrink: 0 }}
+                >
                   {data.title}
                 </Typography>
-                <Typography sx={{ color: "text.secondary" }}>
-                  {data.rating}
+                <Typography className="font-sans text-[#b3f2ff] text-med">
+                  {overallScore}
                 </Typography>
               </AccordionSummary>
-              <AccordionDetails>
-                <p>{data.author}</p>
-                <p>
+              <AccordionDetails className="font-sans text-[#b3f2ff] text-sm">
+                <p className="pb-2">{data.author}</p>
+                <p className="pb-5">
                   Read from: {data.dateStart} - {data.dateFinish}
                 </p>
-                <Typography>{data.review}</Typography>
+                <Typography className="font-mono text-[#b3f2ff] text-med">
+                  {review}
+                </Typography>
               </AccordionDetails>
             </Accordion>
           );
